5eb3a92547ae8e5cd3fa47e7a3436e3978202701
Yura-0 <yurec.name@gmail.com>
2024-07-10 17:29:03 +0300

    add pages and navigation mockup

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'core/routes.dart';
+
 class LanarsApp extends StatelessWidget {
   const LanarsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.auth,
+      routes: AppRoutes.getRoutes(),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
